a9229fcab5fe8145b00d9aa66b3a7d8dbe7427cc
Andrew Seaman <andrewseaman35@gmail.com>
2020-11-27 15:30:55 -0800

    Use better specs for variables

--- src/chip8.h
+++ src/chip8.h
@@ -7,26 +7,26 @@ class Chip8 {
 private:
     uint16_t opcode;
 
-    uint8_t memory[4096]; // "These machines had 4096 (0x1000) memory locations, all of which are 8 bits"
+    uint8_t memory[4096]; // "The Chip-8 language is capable of accessing up to 4KB (4,096 bytes) of RAM"
 
-    uint8_t V[16]; // "CHIP-8 has 16 8-bit data registers named V0 to VF."
-    uint16_t I; // "The address register, which is named I, is 16 bits wide"
+    uint8_t V[16]; // "Chip-8 has 16 general purpose 8-bit registers"
+    uint16_t I; // "There is also a 16-bit register called I"
 
-    uint8_t stack[48]; // "The original RCA 1802 version allocated 48 bytes"
+    uint16_t stack[16]; // "The stack is an array of 16 16-bit values"
+    uint8_t sp; // "The stack pointer (SP) can be 8-bit"
 
-    // CHIP-8 has two timers. They both count down at 60 hertz, until they reach 0.
+    // "Chip-8 also has two special purpose 8-bit registers".
     uint8_t delay_timer;
     uint8_t sound_timer;
 
-    uint16_t pc; // program counter
-
-
+    uint16_t pc; // "The program counter (PC) should be 16-bit"
 
     void init();
+    void cycle();
 
 public:
-    uint8_t display_buffer[64*32]; // Original CHIP-8 Display resolution is 64×32 pixels
-    uint8_t keyboard[16]; // Input is done with a hex keyboard that has 16 keys ranging 0 to F
+    uint8_t display_buffer[64*32]; // "64x32-pixel monochrome display"
+    uint8_t keypad[16]; // "16-key hexadecimal keypad"
 
     void test();
 
